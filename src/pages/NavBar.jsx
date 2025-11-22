7eaa4f0a35bca4c0de5e70df1b56be8c0755ad95
jpablo903 <jpablo_903@hotmail.com>
2025-11-22 14:46:00 -0300

    Fix Narbar

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -133,7 +133,11 @@ function NavBar() {
                 <div className="navbar-icons-section">
                     <div className="nav-carrito-item">
                         <Link to='/Carrito' className={`btn-carrito ${getLinkClass('/Carrito')}`} title="Ir al carrito">
-                            <i className="fas fa-shopping-cart"></i>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-shopping-bag">
+                                <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path>
+                                <line x1="3" y1="6" x2="21" y2="6"></line>
+                                <path d="M16 10a4 4 0 0 1-8 0"></path>
+                            </svg>
                             {itemCount > 0 && <span className="carrito-contador">{itemCount}</span>}
                         </Link>
                     </div>
@@ -148,7 +152,6 @@ function NavBar() {
                                 >
                                     <i className="fas fa-user-circle"></i>
                                 </button>
-
 
                                 {userMenuOpen && (
                                     <div className="user-dropdown-menu">
